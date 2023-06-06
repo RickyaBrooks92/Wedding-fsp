d46fdf42f94a3be160edd9f247bbaef964be03d1
RickyTicky <Rickyabrooks92@gmail.com>
2023-06-06 16:47:15 -0400

    .

--- src/app/components/CameraFeed.js
+++ src/app/components/CameraFeed.js
@@ -2,20 +2,20 @@
 
 import React, { useEffect, useRef } from "react";
 
-const getCamera = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    if (videoRef.current) {
-      videoRef.current.srcObject = stream;
-      videoRef.current.onloadedmetadata = () => videoRef.current.play();
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const CameraFeed = () => {
   const videoRef = useRef(null);
+
+  const getCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.onloadedmetadata = () => videoRef.current.play();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     if (videoRef.current) getCamera();
