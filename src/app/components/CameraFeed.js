123ee87a872d7b61548ceefc89dfd9705ac23db0
RickyTicky <Rickyabrooks92@gmail.com>
2023-06-06 15:42:09 -0400

    updated getCamera function

--- src/app/components/CameraFeed.js
+++ src/app/components/CameraFeed.js
@@ -5,15 +5,10 @@ import React, { useEffect } from "react";
 const CameraFeed = () => {
   const getCamera = async () => {
     try {
-      await navigator.mediaDevices
-        .getUserMedia({
-          video: true,
-        })
-        .then((stream) => {
-          const video = document.querySelector("video");
-          video.srcObject = stream;
-          video.onloadedmetadata = (e) => video.play();
-        });
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const video = document.querySelector("video");
+      video.srcObject = stream;
+      video.onloadedmetadata = () => video.play();
     } catch (error) {
       console.log(error);
     }
