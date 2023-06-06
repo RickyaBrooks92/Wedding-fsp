396c20e9036135d7021a52cea38d97e845f7a130
RickyTicky <Rickyabrooks92@gmail.com>
2023-06-06 16:25:31 -0400

    added canvas for video

--- src/app/components/CameraFeed.js
+++ src/app/components/CameraFeed.js
@@ -1,35 +1,39 @@
-"use client";
+import { useEffect, useRef } from "react";
 
-import React, { useEffect } from "react";
-
-const CameraFeed = () => {
-  const getCamera = async () => {
-    try {
-      await navigator.mediaDevices
-        .getUserMedia({
-          video: true,
-        })
-        .then((stream) => {
-          const video = document.querySelector("video");
-          video.srcObject = stream;
-          video.onloadedmetadata = (e) => video.play();
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+const VideoCanvas = () => {
+  const videoRef = useRef();
+  const canvasRef = useRef();
 
   useEffect(() => {
-    getCamera();
+    // Access the user's camera and stream the video
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      })
+      .catch((error) => {
+        console.log("Error accessing camera:", error);
+      });
+
+    // Draw the video frames onto the canvas
+    const drawVideo = () => {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(drawVideo);
+    };
+
+    // Call the drawVideo function to start drawing the video frames
+    drawVideo();
   }, []);
 
   return (
     <div>
-      <video id="camera-box" muted>
-        {" "}
-      </video>
+      <video ref={videoRef} autoPlay muted />
+      <canvas ref={canvasRef} />
     </div>
   );
 };
 
-export default CameraFeed;
+export default VideoCanvas;
